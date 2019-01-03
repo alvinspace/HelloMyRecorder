3e7638ac156a243da25f08dab3c7dd34a44f73c1
林欣鼎 <winddale@macbook-air.local>
2019-01-03 09:15:37 +0800

    Initial Commit

---
+++ HelloMyRecorder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloMyRecorder
+//
+//  Created by 林欣鼎 on 2019/1/3.
+//  Copyright © 2019年 林欣鼎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
